78e0cee885c267b263029e4581c60b018e4fde46
Scott Tracy <scott.tracy.st@gmail.com>
2018-06-14 13:33:48 -0500

    created project, added addtest

---
+++ CustomListClassTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListClassTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListClassTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42041e20-0514-4642-b0a3-5e5d1c530669")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListClassTest/TestAdd.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CustomListClassTest
+{
+    [TestClass]
+    public class TestAdd
+    {
+        [TestMethod]
+        public void  AddValueToFirstIndex()
+        {
+            //Arrange
+            List<int> list = new List<int>();
+            int value = 5;
+            //Act
+            list.add(value);
+            //Assert
+            Assert.AreEqual(list[0], value);
+        }
+        [TestMethod]
+        public void AddValueToSecondIndex()
+        {
+            //Arrange
+            List<int> list = new List<int>();
+            int firstValue = 5;
+            int secondValue = 7;
+            //Act
+            list.add(firstValue);
+            list.add(secondValue);
+            //Assert
+           
+            Assert.AreEqual(list[0], firstValue);
+        }
+        [TestMethod]
+        public void AddValueToSecondIndex2()
+        {
+            //Arrange
+            List<int> list = new List<int>();
+            int firstValue = 5;
+            int secondValue = 7;
+            //Act
+            list.add(firstValue);
+            list.add(secondValue);
+            //Assert
+            Assert.AreEqual(list[1], secondValue);
+           
+        [TestMethod]
+        public void AddString()
+        {
+            //Arrange
+            List<int> list = new List<int>();
+            int value = 5;
+            //Act
+            list.add(value);
+            //Assert
+            Assert.AreEqual(list[0], value);
+        }
+        [TestMethod]
+        public void AddArray()
+        {
+            //Arrange
+            List<int[]> list = new List<int[]>;
+            int[] array1 = { 1, 2 };
+            int[] array2 = { 3, 4 };
+            //Act
+            list.add(array1);
+            list.add(array2);
+            //Assert
+            Assert.AreEqual(list[0], array1);
+
+        }
+        [TestMethod]
+        public void AddArrayCheck2()
+        {
+            //Arrange
+            List<int[]> list = new List<int[]>;
+            int[] array1 = { 1, 2 };
+            int[] array2 = { 3, 4 };
+            //Act
+            list.add(array1);
+            list.add(array2);
+            //Assert
+            Assert.AreEqual(list[1], array2);
+
+        }
+
+    }
+}
